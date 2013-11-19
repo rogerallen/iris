34d1da3f4501992a347654afddd89c8b05dc602d
Roger Allen <rallen@gmail.com>
2013-11-18 23:15:48 -0800

    add test

--- test/iris/core_test.clj
+++ test/iris/core_test.clj
@@ -1,7 +1,36 @@
 (ns iris.core-test
   (:require [clojure.test :refer :all]
-            [iris.core :refer :all]))
+            [iris.pipeline :as iris]
+            [iris.matrix :as mat]
+            [iris.util :as util]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest simple-test
+  (testing "simple usage of iris"
+    (let [dim 64
+          objects [{:type     :triangle-list
+                    :vertices [{:x -1.0 :y -1.0 :z 0.0 :r 1.0 :g 0.0 :b 0.0}
+                               {:x  1.0 :y -1.0 :z 0.0 :r 0.0 :g 1.0 :b 0.0}
+                               {:x  1.0 :y  1.0 :z 0.0 :r 0.0 :g 0.0 :b 1.0}
+                               ;; fullscreen tri
+                               {:x -3.0 :y -1.0 :z 0.5 :r 1.0 :g 1.0 :b 1.0}
+                               {:x  1.0 :y -1.0 :z 0.5 :r 1.0 :g 1.0 :b 1.0}
+                               {:x  1.0 :y  3.0 :z 0.5 :r 1.0 :g 1.0 :b 1.0}
+                               ]}]
+          state {:viewport          [0 0 dim dim]
+                 :fbport            [0 0 dim dim]
+                 :depth-range       [0.0 1.0]
+                 :view-matrix       (mat/identity-matrix)
+                 :model-matrix      (mat/identity-matrix)
+                 :projection-matrix (mat/identity-matrix)
+                 }
+          framebuffer {:x      0
+                       :y      0
+                       :width  dim
+                       :height dim
+                       :data   (vec (repeat (* dim dim)
+                                            {:r 0 :g 0 :b 0 :z 1}))
+                       }
+          framebuffer (iris/render-framebuffer state framebuffer objects)
+          ;;_ (util/print-fb-to-ppm framebuffer)
+          crc (util/check-crc framebuffer)]
+      (is (= crc 3771693458)))))
