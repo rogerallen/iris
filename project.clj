4d3b7b6d8512ccc6b0d63acd2f71a39ccfd3c8bd
Roger Allen <rallen@gmail.com>
2016-01-21 06:37:02 -0800

    update to clojure 1.8.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url          "http://github.com/rogerallen/iris"
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"] ;; works for 1.6.0, 1.7.0, 1.8.0
                  [primitive-math      "0.1.4"]]
   :main         ^:skip-aot iris.core
   :target-path  "target/%s"
